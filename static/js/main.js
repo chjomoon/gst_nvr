4d60cca44a4a75be0518093273af885c50475466
chjomoon <33528293+chjomoon@users.noreply.github.com>
2020-06-04 17:21:23 +0900

    function edited

--- static/js/main.js
+++ static/js/main.js
@@ -12,7 +12,30 @@ jQuery(function ($){
     $("#datetimepicker-camera").on("dp.change", function (e){
 	loadVideo(e.date.format('YYYYMMDD'));
     });
+    
+    $('#stream').on("click", function(){
+      var live_url = 'http://192.168.0.237:8000/test/hls/index.m3u8'
+      var live = document.getElementById('hls');
+      if (live.canPlayType('application/vnd.apple.mpegurl')){
+         live.src = vide_url;
+         console.log("stream play")
+         live.addEventListener('loadedmetadata',function(){
+  		    live.play();
+  	    });
+           }else{
+        var player = new Hls();
+        console.log("stream play")
+        player.loadSource(live_url);
+        player.attachMedia(live)
+        player.on(Hls.Events.MANIFEST_PARSED,function(){
+          
+		      live.play();
+      });
+      }       
+    });
+   
 
+    
     function loadVideo(date) {
 	    var cameraRoute = '/home/imr/nms/Node-Media-Server/public/test/'+date + '/index.m3u8';
 	    var video = document.getElementById('video');
